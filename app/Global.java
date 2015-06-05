7668bbb7538f891df6a31edd711acb25033a6bb9
Yuan Zhencai <zhencai.yuan@51jdj.com>
2015-06-06 03:57:03 +0800

    global

--- app/Global.java
+++ app/Global.java
@@ -2,6 +2,11 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import play.GlobalSettings;
+import play.libs.F;
+import play.mvc.Http;
+import play.mvc.Result;
+
+import static play.mvc.Results.ok;
 
 /**
  * Created by Yuan on 2015/6/6.
@@ -21,4 +26,13 @@ public class Global  extends GlobalSettings{
     public <A> A getControllerInstance(Class<A> aClass) throws Exception {
         return injector.getInstance(aClass);
     }
+
+    public F.Promise<Result> onError(Http.RequestHeader request, Throwable t) {
+        return F.Promise.<Result>pure(ok(views.html.loveStory.render()));
+    }
+
+    public F.Promise<Result> onHandlerNotFound(Http.RequestHeader request) {
+        return F.Promise.<Result>pure(ok(views.html.loveStory.render()));
+    }
+
 }
